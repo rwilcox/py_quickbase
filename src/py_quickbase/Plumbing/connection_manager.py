e45dc8d115633c27a6bddb90eaa82b3bb8e93245
Ryan Wilcox <rwilcox@wilcoxd.com>
2009-07-26 10:47:50 -0400

    private methods marked as private, started auth stuff...

--- src/py_quickbase/Plumbing/connection_manager.py
+++ src/py_quickbase/Plumbing/connection_manager.py
@@ -1,8 +1,9 @@
+import xml.dom
 
 class ConnectionManager(object):
     """ConnectionManager takes care of the connecting to QuickBase and dealing with responses"""
     
-    def __init__(self, domain, user, password, dbid, app_token, app_name):
+    def __init__(self, domain, user, password, dbid, app_name, app_token = None):
         super(ConnectionManager, self).__init__()
         self.domain = domain
         self.user = user
@@ -11,17 +12,28 @@ class ConnectionManager(object):
         self.app_token = app_token
         self.app_name = app_name
     
-    def fetch_from(self, url_after_db_id):
+    def _fetch_from(self, url_after_db_id):
         """Retrieves the data from the URL. """
         pass
     
-    def handle_response(self):
+    def _handle_response(self):
         pass
     
-    def fetch_and_handle_response(self):
+    def _fetch_and_handle_response(self):
         """Convenience method: do the fetching and the response handling/parsing in method"""
         return self.handle_response( self.fetch() )
     
-    def _base_url(self):
-        return "https://%(domain)s.quickbase.com/%(dbid)s" % dict(domain=self.domain, dbid=self.dbid)
+    def _base_url(self, withDBID=True):
+        if withDBID:
+            return "https://%(domain)s.quickbase.com/%(dbid)s" % dict(domain=self.domain, dbid=self.dbid)
+        else
+            return "https://%(domain)s.quickbase.com/"
+    
+    # ======================================================
+    # Quickbase API Methods
+    # =====================================================
+    def authenticate(self):
+        """You must log in before you make a call to QuickBase"""
+        url = self._base_url(False) + "db/main"
+        
     
